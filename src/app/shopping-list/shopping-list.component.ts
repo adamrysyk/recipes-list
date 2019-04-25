1ab1196491d0aa8914420a5a6a7107c8f32c6679
adamrysyk <rysyk.adam@gmail.com>
2019-04-24 22:01:34 -0400

    add shopping list html structure and an ingredient model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
     selector: 'app-shopping-list',
     templateUrl: './shopping-list.component.html',
     styleUrls: ['./shopping-list.component.sass']
 })
 export class ShoppingListComponent implements OnInit {
+    ingredients: Ingredient[] = [
+        new Ingredient('Eggs', 3),
+        new Ingredient('Rice', 1),
+        new Ingredient('Holy Basil', 2)
+    ];
+
     constructor() { }
 
     ngOnInit() {
